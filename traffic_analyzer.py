b0ca9ca51f6f96d5f85fcd716d670a60240a4031
alexpmr <136476877+alexpmr@users.noreply.github.com>
2026-09-23 23:47:34 -0300

    Atualiza núcleo para v1.20.0

--- traffic_analyzer.py
+++ traffic_analyzer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Traffic Analyzer v1.19.0
+Traffic Analyzer v1.20.0
 
 - Analisa traceroutes do MeshMonitor e descobre nós intermediários.
 - Solicita NodeInfo de nós desconhecidos/incompletos com cooldown.
