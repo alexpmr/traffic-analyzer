7701d383b52bc8840c420c6d91bbc9c9145094fe
alexpmr <136476877+alexpmr@users.noreply.github.com>
2026-09-25 02:39:03 -0300

    Atualiza traffic_analyzer.py para v1.24.1

--- traffic_analyzer.py
+++ traffic_analyzer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Traffic Analyzer v1.24.0
+Traffic Analyzer v1.24.1
 
 - Analisa traceroutes do MeshMonitor e descobre nós intermediários.
 - Solicita NodeInfo de nós desconhecidos/incompletos com cooldown.
